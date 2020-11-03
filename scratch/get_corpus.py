f9fac10eff2cada4c9148d90ef33d6a5cfdc9156
yonghee12 <suncar1000@gmail.com>
2020-11-03 18:52:27 +0900

    add load feature

--- scratch/get_corpus.py
+++ scratch/get_corpus.py
@@ -4,14 +4,15 @@ from pathlib import Path
 ROOT = str(Path(__file__).parent.parent.absolute())
 sys.path.insert(0, ROOT)
 
-from modules import *
+from doc_embedder.modules import *
 
-filepath = os.path.join(ROOT, 'data', 'tb_watcha_comments_top30.csv')
-df = pd.read_csv(filepath, header=0, index_col=0, encoding='utf8')
-print(df.head())
-df = df.dropna(axis=0, inplace=False)
-df.reset_index(drop=True, inplace=True)
 
-df.groupby(by='title', axis=0).count().sort_values(by='text', ascending=False)
-
-df.sample(n=50)
+def load_watcha_sample_corpus(n_sample=None):
+    filepath = os.path.join(ROOT, 'data', 'tb_watcha_comments_top30.csv')
+    df = pd.read_csv(filepath, header=0, index_col=0, encoding='utf8')
+    df.dropna(axis=0, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    if isinstance(n_sample, int):
+        if n_sample >= 0:
+            return df.sample(n=n_sample)
+    return df
