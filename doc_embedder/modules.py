447715226b1bf553561d94b5e52977478e64b594
yonghee12 <suncar1000@gmail.com>
2020-11-05 14:09:49 +0900

    add import modules

--- doc_embedder/modules.py
+++ doc_embedder/modules.py
@@ -1,2 +1,12 @@
+import sys
+from pathlib import Path
 import pandas as pd
 import os
+import re
+from gensim import corpora, models
+import logging
+from pprint import pprint
+from konlpy.tag import Mecab
+
+ROOT = str(Path(__file__).parent.parent.absolute())
+sys.path.insert(0, ROOT)
