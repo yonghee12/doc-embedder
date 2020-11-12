c4f1c73fb5e793612856948671b5b3304ea3616f
yonghee12 <suncar1000@gmail.com>
2020-11-12 10:37:49 +0900

    add loadw2v

---
+++ scratch/load_w2v.py
@@ -0,0 +1,39 @@
+import sys
+from pathlib import Path
+
+ROOT = str(Path(__file__).parent.parent.absolute())
+sys.path.insert(0, ROOT)
+
+import pickle
+import pandas as pd
+import numpy as np
+import gensim.models
+from gensim.models.callbacks import CallbackAny2Vec
+from string import punctuation
+from time import perf_counter as now
+from direct_redis import DirectRedis
+from tqdm import tqdm
+from progress_timer import Timer
+
+from _config import *
+from doc_embedder.modules import *
+from doc_embedder.functions import *
+
+r = DirectRedis(**BOOK_ID_MAPPING)
+
+source_dir = '/home/ubuntu/yonghee/doc-embedder/book_nouns'
+agg_filename = 'book_nouns_agg.txt'
+agg_filepath = os.path.join(source_dir, agg_filename)
+
+agg_filename_sample = 'book_nouns_agg_sample.txt'
+agg_filepath_sample = os.path.join(source_dir, agg_filename_sample)
+
+model_dir = '/home/ubuntu/yonghee/doc-embedder/trained_models'
+
+modelfname = 'w2v_20031_50d_epoch1_loss488617.model'
+modelpath = os.path.join(model_dir, modelfname)
+model2 = gensim.models.Word2Vec.load(modelpath)
+# model2 = gensim.models.Word2Vec.load(modelpath, encoding='utf8')
+print(model2.wv.most_similar('사랑'))
+print('hello')
+print('asdfadsf')
